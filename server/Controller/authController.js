bdf765df36646110d29e0e3ef4755c5923ded949
Naeem Khan <naeem55001@gmail.com>
2025-10-15 00:48:49 +0500

    batman

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -1,4 +1,62 @@
-const loginController = async (req, res) => { };
-const registerController = async (req, res) => { };
+const firmaUser = require("../Model/firma_user.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const registerController = async (req, res) => {
+	const { email, password, role } = req.body;
+
+	if (!email || !password) throw new Error("email or password missing");
+
+	try {
+		const already_user = await firmaUser.findOne({ email });
+		if (already_user) throw new Error("User already in database");
+		console.log("one");
+		const hash_password = await bcrypt.hash(password, 10);
+		console.log(hash_password);
+		const newUser = await firmaUser.create({
+			email,
+			password: hash_password,
+			role,
+		});
+		console.log("three");
+		res.json({
+			message: "suckseeded",
+			newUser,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const loginController = async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) throw new Error("missing email | password");
+
+	try {
+		const user_exist = await firmaUser.findOne({ email });
+		if (!user_exist) throw new Error("there is no user like this");
+
+		const password_compare = await bcrypt.compare(
+			password,
+			user_exist.password,
+		);
+
+		if (!password_compare) throw new Error("the passowrd is wrong");
+
+		const token = jwt.sign(
+			{ email: user_exist.email, id: user_exist._id, role: user_exist.role },
+			process.env.SECRET,
+			{ expiresIn: "9d" },
+		);
+
+		res.send({
+			message: "suckseed",
+			token,
+			user_exist,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 module.exports = { loginController, registerController };
--- server/Model/firma_user.js
+++ server/Model/firma_user.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		required: true,
+		default: "user",
+	},
 });
 
 module.exports = mongoose.model("firmaUser", Schema);
